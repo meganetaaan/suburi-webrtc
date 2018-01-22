02e1bfe81c749200e438be358c70b7384b9e6379
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-01-23 02:18:09 +0900

    Refactor project

--- examples/one-to-many/src/app.js
+++ examples/one-to-many/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const socketio = require('socket.io')
+const path = require('path')
 
 // Socket IO Server
 const port = 9001
@@ -7,14 +8,18 @@ const io = socketio.listen(port)
 console.log((new Date()) + ' Server is listening on port ' + port)
 io.sockets.on('connection', socket => {
   socket.on('message', function (message) {
+    console.log(message)
     socket.broadcast.emit('message', message)
   })
   socket.on('disconnect', socket => {
-    socket.broadcast.emit('user disconnected')
+    console.log('disconnected')
+    // socket.broadcast.emit('user disconnected')
   })
 })
 
 // Express Server
 const app = express()
-app.use(express.static('public'))
-app.listen(3000, () => { console.debug('listening on port 3000') })
+const staticPath = path.join(__dirname, '/public')
+console.log(staticPath)
+app.use(express.static(staticPath))
+app.listen(3000, () => { console.log('listening on port 3000') })
